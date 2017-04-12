edb57cb92e26ba0650294769c2a29c1070dba39f
NisseAnd <anders-nissen@stofanet.dk>
2017-04-12 14:50:32 +0200

    har oprettet en Storage Constructor klasse

---
+++ FOG/src/main/java/entity/Storage.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Anders
+ */
+public class Storage {
+    private int id;
+    private String description;
+    private String type;
+    private String name;
+
+    public Storage(int id, String description, String type, String name) {
+        this.id = id;
+        this.description = description;
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Storage{" + "id=" + id + ", description=" + description + ", type=" + type + ", name=" + name + '}';
+    }
+}
