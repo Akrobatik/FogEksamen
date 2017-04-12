51a25c9b67781f15b42f09d5683c105cf08d92bf
NisseAnd <anders-nissen@stofanet.dk>
2017-04-13 01:37:25 +0200

    har lavet en ny overloaded metode i Order klassen, og lavet getOrder metoden færdig i UserAdminMapper

--- FOG/src/main/java/mapper/UserAdminMapper.java
+++ FOG/src/main/java/mapper/UserAdminMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import db.DataBase;
+import entity.Order;
 import entity.User;
 import entity.User2;
 import entity.UserAdmin;
@@ -48,6 +49,23 @@ public class UserAdminMapper {
                 users.add(u);
             }
             return users;
+        } catch (SQLException ex) {
+            throw new ToLogException("" + ex.getMessage());
+        }
+    }
+    
+    public List<Order> getOrders() throws ToLogException {
+        List<Order> order = new ArrayList<>();
+        try {
+            String sql = "select roofType, width, length, heigth from Order";
+            
+            PreparedStatement pstmt = DataBase.getConnection().prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                Order o = new Order(rs.getString("roofType"), rs.getDouble("width"), rs.getDouble("length"), rs.getDouble("heigth"));
+                order.add(o);
+            }
+            return order;
         } catch (SQLException ex) {
             throw new ToLogException("" + ex.getMessage());
         }
