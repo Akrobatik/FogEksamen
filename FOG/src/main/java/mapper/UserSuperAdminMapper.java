3a8051ceb04363954a65ac428fd670b18ff74ae1
NisseAnd <anders-nissen@stofanet.dk>
2017-04-12 16:58:58 +0200

    oprettede validateUser metode for UserSuperAdminMapper

--- FOG/src/main/java/mapper/UserSuperAdminMapper.java
+++ FOG/src/main/java/mapper/UserSuperAdminMapper.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mapper;
+
+import db.DataBase;
+import entity.UserSuperAdmin;
+import exceptions.UserFeedbackException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author Anders
  */
 public class UserSuperAdminMapper {
-    
+    public UserSuperAdmin validateSuperAdmin(String username, String password) throws UserFeedbackException {
+        try {
+            String sql = "select * from SuperAdmin where username = ? and password = ?";
+            
+            PreparedStatement pstmt = DataBase.getConnection().prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if(rs.next()) {
+                int idSuperAdmin = rs.getInt("idSuperAdmin");
+                return new UserSuperAdmin(idSuperAdmin, username, password);
+            }
+        } catch (SQLException ex) {
+            throw new UserFeedbackException("Username or password did not match" + ex.getMessage());
+        }
+        return null;
+    }
 }
