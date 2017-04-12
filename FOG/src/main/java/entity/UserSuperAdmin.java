b02166acf97d06e14cdbe8c39d5d7784d2a3a9d2
NisseAnd <anders-nissen@stofanet.dk>
2017-04-12 16:53:27 +0200

    oprettede en UserSuperAdmin Contructor klasse

--- FOG/src/main/java/entity/UserSuperAdmin.java
+++ FOG/src/main/java/entity/UserSuperAdmin.java
@@ -10,5 +10,42 @@ package entity;
  * @author Anders
  */
 public class UserSuperAdmin {
-    
+    private int idSuperAdmin;
+    private String username;
+    private String password;
+
+    public UserSuperAdmin(int idSuperAdmin, String username, String password) {
+        this.idSuperAdmin = idSuperAdmin;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getIdSuperAdmin() {
+        return idSuperAdmin;
+    }
+
+    public void setIdSuperAdmin(int idSuperAdmin) {
+        this.idSuperAdmin = idSuperAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSuperAdmin{" + "idSuperAdmin=" + idSuperAdmin + ", username=" + username + ", password=" + password + '}';
+    }
 }
