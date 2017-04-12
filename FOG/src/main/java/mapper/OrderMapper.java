235c6d3fabcd570ddc34c87f433fbd0f0a29b8c6
NisseAnd <anders-nissen@stofanet.dk>
2017-04-13 01:42:40 +0200

    har lavet en OrderMapper klasse, og har flyttet getOrder metoden over i den anførte klasse

--- FOG/src/main/java/mapper/OrderMapper.java
+++ FOG/src/main/java/mapper/OrderMapper.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mapper;
+
+import db.DataBase;
+import entity.Order;
+import exceptions.ToLogException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Anders
  */
 public class OrderMapper {
-    
+    public List<Order> getOrders() throws ToLogException {
+        List<Order> order = new ArrayList<>();
+        try {
+            String sql = "select roofType, width, length, heigth from Order";
+            
+            PreparedStatement pstmt = DataBase.getConnection().prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                Order o = new Order(rs.getString("roofType"), rs.getDouble("width"), rs.getDouble("length"), rs.getDouble("heigth"));
+                order.add(o);
+            }
+            return order;
+        } catch (SQLException ex) {
+            throw new ToLogException("" + ex.getMessage());
+        }
+    }
 }
